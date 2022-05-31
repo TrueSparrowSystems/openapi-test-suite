4d854c7b3010633c3fcdf4735a1cf43500658892
V_R_Dighe <vaibhav@moxie.xyz>
2022-05-31 17:19:34 +0530

    Code refactoring.

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ const rootPrefix = '.',
  * @class ApiTestSuite
  */
 class ApiTestSuite {
-  constructor(openapiObj, serverIndex) {
+  constructor(openapiObj, serverIndex, securityInfo) {
     const oThis = this;
 
     oThis.openapiObj = openapiObj;
     oThis.serverIndex = serverIndex;
+    oThis.securityInfo = securityInfo;
 
     // Saving the params in-memory via configProvider
-    configProvider.setConfig('openapiObj', openapiObj);
-    configProvider.setConfig('serverIndex', serverIndex);
+    configProvider.setConfig('openapiObj', oThis.openapiObj);
+    configProvider.setConfig('serverIndex', oThis.serverIndex);
+    configProvider.setConfig('securityInfo', oThis.securityInfo);
   }
 
   /**
@@ -27,21 +29,10 @@ class ApiTestSuite {
    * @return {Promise<void>}
    */
   runTest() {
-    const securityInfo = {
-      requestHeaders: {},
-      userAuth: {
-        cookies: {
-          aulc: {
-            Name: 'aulc',
-            Value:
-              's%3A1%3Aapp%3A1%3Aphone%3A1653897346.477%3A11e495083f8a1bb338ce6e10d9f2eed39252521d84e75aa5bf254c97911d9550.962O00SLpO0mEmsypUGDglrN7wgoItB0EKhSxe6t0c8'
-          }
-        }
-      }
-    };
+    const oThis = this;
 
     new StartSuite({
-      securityInfo
+      securityInfo: oThis.securityInfo
     }).perform();
   }
 
